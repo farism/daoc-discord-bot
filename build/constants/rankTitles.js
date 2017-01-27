a764d3da0408e2858364109f0fcb3d68c2a3427b
Faris Mustafa <farismmk@gmail.com>
2017-01-27 14:09:16 -0800

    remove build folder

--- build/constants/rankTitles.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = [['Guardian', 'Savant', 'Skiltvakten'], ['Warder', 'Consantoir', 'Isen Vakten'], ['Myrmidon', 'Brehon', 'Flammen Vakten'], ['Gryphon Knight', 'Grove Protector', 'Elding Vakten'], ['Eagle Knight', 'Raven Ardent', 'Stormur Vakten'], ['Phoenix Knight', 'Silver Hand', 'Isen Herra'], ['Alerion Knight', 'Thunderer', 'Flammen Herra'], ['Unicorn Knight', 'Gilded Spear', 'Elding Herra'], ['Lion Knight', 'Tiarna', 'Bantiarna', 'Stormur Herra'], ['Dragon Knight', 'Emerald Ridere', 'Einherjar'], ['Lord', 'Lady', 'Barun', 'Banbharun', 'Herra', 'Fru'], ['Baronet', 'Baronetess', 'Ard Tiarna', 'Ard Bantiarna', 'Hersir', 'Baronsfru'], ['Baron', 'Baroness', 'Ciann', 'Cath', 'Vicomte', 'Vicomtessa'], ['Arch Duke', 'Arch Duchess', 'Ard Diuc', 'Ard Bandiuc', 'Stor Jarl', 'Stor Hurfru']];
